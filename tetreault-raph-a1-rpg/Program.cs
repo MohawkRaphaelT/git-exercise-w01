89026c25cbd4b94e265863ebbc0944b343e3e751
MohawkRaphaelT <150467892+MohawkRaphaelT@users.noreply.github.com>
2024-05-07 11:13:09 -0400

    Change program to small IO example

--- tetreault-raph-a1-rpg/Program.cs
+++ tetreault-raph-a1-rpg/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Raph!");
+            string userInput = Console.ReadLine();
+            Console.WriteLine($"You said: \"{userInput}\"!");
+            Console.Read();
         }
     }
 }
